4f41e0fe1b054a1e3e4188a17c0bde30f1aeeec9
Matteo Abis <1423701+Enucatl@users.noreply.github.com>
2025-04-17 15:26:14 +0200

    use online models from deepgram

--- main.py
+++ main.py
@@ -1,47 +1,32 @@
-import numpy as np
-from transformers import pipeline
 import click
-import pydub
-
-
-def pydub_to_np(audio: pydub.AudioSegment) -> np.ndarray:
-    """
-    Converts pydub audio segment into np.float32 of shape [duration_in_seconds*sample_rate, channels],
-    where each value is in range [-1.0, 1.0].
-    Returns tuple (audio_np_array, sample_rate).
-    """
-
-    # Get the raw audio data as an array of bytes
-    samples = audio.get_array_of_samples()
-
-    # Convert to numpy array
-    samples_np = np.array(samples).astype(np.int16)
-
-    # Normalize to range [-1, 1]
-    samples_np = samples_np / np.iinfo(samples_np.dtype).max
-
-    return samples_np
+import httpx
+from deepgram import DeepgramClient, PrerecordedOptions, FileSource
 
 
 @click.command()
-@click.argument("input_file", type=click.Path(exists=True))
-def main(input_file):
-    device = "cuda:0"
-    pipe = pipeline(
-        "automatic-speech-recognition", model="openai/whisper-large-v3", device=device
+@click.option("--input_file", type=click.File("rb"), required=True)
+@click.option("--api_key", required=True)
+@click.option("--model", default="nova-2")
+@click.option("--language", default="it")
+@click.option("--smart_format/--no_smart_format", default=True)
+def _main(
+    input_file: click.File, api_key: str, model: str, language: str, smart_format: bool
+) -> None:
+    source: FileSource = {"buffer": input_file}
+    options = PrerecordedOptions(
+        model=model,
+        language=language,
+        smart_format=smart_format,
     )
-    sound = pydub.AudioSegment.from_mp3(input_file)
-    sound = sound.set_channels(1).set_frame_rate(16_000)
-    sound_array = pydub_to_np(sound)
-    result = pipe(
-        sound_array,
-        max_new_tokens=256,
-        generate_kwargs={"task": "transcribe"},
-        chunk_length_s=30,
-        batch_size=2,
+    deepgram = DeepgramClient(api_key)
+    response = deepgram.listen.rest.v("1").transcribe_file(
+        source,
+        options,
+        timeout=httpx.Timeout(300, connect=10),
     )
-    print(result)
+    # print(response.to_json(indent=2))
+    print(response["results"]["channels"][0]["alternatives"][0]["transcript"])
 
 
 if __name__ == "__main__":
-    main()
+    _main()
